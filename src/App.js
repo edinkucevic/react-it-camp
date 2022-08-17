b996c7206b843a458fdd4082a89c96ce04521016
Edin Kucevic <edinkucevic99@gmail.com>
2022-08-17 20:14:29 +0200

    react

--- src/App.js
+++ src/App.js
@@ -24,53 +24,135 @@
 
 // export default App;
 
-import "./App.css";
-import React from "react";
-import Increamenter from "./components/Increamenter/Increamenter";
+// import "./App.css";
+// import React from "react";
+// import Increamenter from "./components/Increamenter/Increamenter";
 
-export default function App() {
-  const[count, setCount] = React.useState(0);
-  const handleClick = () => {
-    setCount((prev) => prev + 1)
-  }
+// export default function App() {
+//   const[count, setCount] = React.useState(0);
+//   const handleClick = () => {
+//     setCount((prev) => prev + 1)
+//   }
 
 
-  const sayHello = (name) => {
-    alert(`Hello ${name}`)
-  };
+//   const sayHello = (name) => {
+//     alert(`Hello ${name}`)
+//   };
  
-  //  let count = 0;
+//   //  let count = 0;
 
 
-  return (
-    <div className="card-container">
-      {count}
+//   return (
+//     <div className="card-container">
+//       {count}
 
 
-      <button 
-      onClick = {handleClick}
-      // onClick={() => {
-      //   count++
-      //   console.log(count);
-      // }}
-      >Increase
-      </button> 
+//       <button 
+//       onClick = {handleClick}
+//       // onClick={() => {
+//       //   count++
+//       //   console.log(count);
+//       // }}
+//       >Increase
+//       </button> 
 
       
-      {/* <button onClick={() => sayHello('Edin')}>Click me</button>
-      <button 
-       onClick={() => {
-         sayHello("Rick");
-         console.log("Neka nova funkcija nije definisana");
-       }}
-       >
-         In line anonimna funkcija
-       </button> */}
-       <Increamenter />
+//       {/* <button onClick={() => sayHello('Edin')}>Click me</button>
+//       <button 
+//        onClick={() => {
+//          sayHello("Rick");
+//          console.log("Neka nova funkcija nije definisana");
+//        }}
+//        >
+//          In line anonimna funkcija
+//        </button> */}
+//        <Increamenter />
+//     </div>
+//   )
+// }
+
+
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+ export default function App() {
+
+  // const [name , setName] = React.useState("")
+  // const [email , setEmail] = React.useState("")
+  // const [hobi , setHobi] = React.useState("")
+  // const [password, setPassword] = React.useState("")
+
+  const [formValues , setFormValues] = useState({
+    name: "" ,
+    email: "",
+    hobi: "",
+    password: "",
+    
+  })
+
+
+
+    return(
+      <div className="card-container">
+      <form onSubmit={(e) => { 
+      e.preventDefault();
+      console.log(formValues)
+      console.log(`NAME: ${formValues.name} EMAIL: ${formValues.email} HOBI: ${formValues.hobi} PASSWORD: ${formValues.password}`)
+      }}>
+        <label htmlFor="html">Name</label>
+        <input 
+        type="text" 
+        id="html"
+        name="fav_language"
+        value={formValues.name}
+        onChange={(event) => setFormValues(prev => ({
+           
+          ...prev,
+          name: event.target.value,
+        }))
+      }
+        />
+        <br />
+        <label htmlFor="css">email</label>
+        <input 
+        type="text"
+        id="css"
+        name="fav_language"
+         value={formValues.email}
+         onChange={(event) => setFormValues(prev => ({
+           
+          ...prev,
+          name: event.target.value,
+        }))
+      }
+         />
+        <br />
+        <label htmlFor="javascript">Hobi</label>
+        <input 
+        type="text"
+        id="javascript"
+        name="fav_language" 
+        value={formValues.hobi}    
+        onChange={(event) => setFormValues(prev =>({
+          ...prev,
+          hobi: event.target.value,
+          
+
+          }
+        ))}   
+         />
+        <br />
+        <label htmlFor="password">sifra</label>
+        <input 
+        type="text"
+        id="password"
+        name="fav_language" 
+        // value={password}
+        // onChange={(event) => setPassword(event.target.value)}
+        />
+        <br />
+        <input type="submit" defaultValue="Submit" />
+      </form>
     </div>
-  )
+    );
 }
-
-
-
-
