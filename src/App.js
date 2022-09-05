8b6b472f7f071377dabcc66a39363838271164eb
Edin Kucevic <edinkucevic99@gmail.com>
2022-09-05 18:54:54 +0200

    material ui

--- src/App.js
+++ src/App.js
@@ -182,62 +182,148 @@
 // export default App;
 
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import "./App.css";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
  
-const BASE_URL = "https://api.quotable.io";
+// const BASE_URL = "https://api.quotable.io";
  
-export default function App() {
-  const [authors, setAuthors] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [pagination, setPagination] = useState({});
-  const [page, setPage] = useState(1);
+// export default function App() {
+//   const [authors, setAuthors] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [pagination, setPagination] = useState({});
+//   const [page, setPage] = useState(1);
  
-  function getAuthors(page) {
-    setLoading(true);
-    try {
-      axios
-        .get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}&limit=15`)
-        .then((res) => {
-          console.log(res.data);
-          setPagination({
-            page: res.data.page,
-            lastPage: res.data.totalPages,
-          });
-          setAuthors(res.data.results);
-        });
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoading(false);
-    }
-  }
+//   function getAuthors(page) {
+//     setLoading(true);
+//     try {
+//       axios
+//         .get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}&limit=15`)
+//         .then((res) => {
+//           console.log(res.data);
+//           setPagination({
+//             page: res.data.page,
+//             lastPage: res.data.totalPages,
+//           });
+//           setAuthors(res.data.results);
+//         });
+//     } catch (e) {
+//       console.log(e);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }
  
-  useEffect(() => {
-    getAuthors(page);
-  }, [page]);
+//   useEffect(() => {
+//     getAuthors(page);
+//   }, [page]);
  
+//   return (
+//     <div className="card-container">
+//       Page {pagination.page} / {pagination.lastPage}
+//       <button onClick={() => setPage((prev) => prev + 1)}>Next page </button>
+//       {!loading ? (
+//         <div>
+//           {authors.map((author) => (
+//             <div key={author._id}>
+//               <h4>{author.name}</h4>
+//               <h5>{author.description}</h5>
+//               <p>{author.bio}</p>
+//               <p>Quoute count: {author.quoteCount}</p>
+//               <hr />
+//             </div>
+//           ))}
+//         </div>
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   );
+// }
+ 
+
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+
+export default function AlignItemsList() {
   return (
-    <div className="card-container">
-      Page {pagination.page} / {pagination.lastPage}
-      <button onClick={() => setPage((prev) => prev + 1)}>Next page </button>
-      {!loading ? (
-        <div>
-          {authors.map((author) => (
-            <div key={author._id}>
-              <h4>{author.name}</h4>
-              <h5>{author.description}</h5>
-              <p>{author.bio}</p>
-              <p>Quoute count: {author.quoteCount}</p>
-              <hr />
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Sandra Adams
+              </Typography>
+              {' — Do you have Paris recommendations? Have you ever…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
   );
 }
- 
+
+
